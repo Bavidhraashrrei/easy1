d526d4fd9814d6b0fbeb54a8f5f73f30fab13a0b
Bavi <bavidhraa.shrrei@kanini.com>
2021-01-27 23:17:13 +0530

    Add project files.

---
+++ EmployeePromotion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace EmployeePromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employeedetails em = new employeedetails();
+            Console.WriteLine("Please enter the employee names in the order of their eligibility for promotion");
+            em.Add_employee_name();
+        }
+    }
+}
---
+++ EmployeePromotion/employeedetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePromotion
+{
+   public class employeedetails
+    {
+        List<string> emp_name = new List<string>();
+        string name;
+        public void Add_employee_name()
+        {
+            do
+            {
+                name = Console.ReadLine();
+                emp_name.Add(name);
+            } while (!string.IsNullOrEmpty(name));
+        }
+     }
+}
